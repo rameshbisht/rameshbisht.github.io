9f0a5c1f99fe163f10fdfd5f0d7e58b6b422c901
Nikhil Singh Bisht <nikhilsinghbisht21@gmail.com>
2019-04-15 11:57:45 +0530

    Fixing animation issues

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+var tog = false;
 
-var slide=()=>{
+var slide = () => {
     alert('Hello')
-    $("#mainWindow").animate({left:'-500px'});
+    $("#mainWindow").animate({ left: '-500px' });
+}
+
+var animate = () => {
+    alert('Animated')
+
+    if (tog) {
+        $("p").animate({ left: "+=100px" });
+    } else {
+        $("p").animate({ left: "+=100px" });
+    }
+
+    tog = !tog;
 }
