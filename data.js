5d26e1cb95cd57731dc59d20a3d41f5e2e18227a
nikhilbisht21 <nikhilsinghbisht21@gmail.com>
2020-10-29 23:09:34 +0530

    Added new images, added new library for gallery

---
+++ data.js
@@ -0,0 +1,391 @@
+data = [
+  {
+    "thumbnail": "res\\1\\1.jpg",
+    "title": "Morni Hills, Panchkula",
+    "slides": [
+      { "src": "res\\1\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\1\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\1\\3.jpg", "w": 720, "h": 960 },
+      { "src": "res\\1\\4.jpg", "w": 960, "h": 720 },
+      { "src": "res\\1\\5.jpg", "w": 960, "h": 720 },
+      { "src": "res\\1\\6.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\2\\1.jpg",
+    "title": "Municipal Corporation, Dharamshala",
+    "slides": [
+      { "src": "res\\2\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\2\\2.jpg", "w": 960, "h": 540 },
+      { "src": "res\\2\\3.jpg", "w": 1200, "h": 680 }
+    ]
+  },
+  {
+    "thumbnail": "res\\3\\1.jpg",
+    "title": "Showroom, PWD Workshop, Shamshi Kullu, Himachal",
+    "slides": [
+      { "src": "res\\3\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\3\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\3\\3.jpg", "w": 960, "h": 720 },
+      { "src": "res\\3\\4.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\4\\1.jpg",
+    "title": "Mandi, Himachal",
+    "slides": [
+      { "src": "res\\4\\1.jpg", "w": 960, "h": 540 },
+      { "src": "res\\4\\2.jpg", "w": 960, "h": 540 },
+      { "src": "res\\4\\3.jpg", "w": 960, "h": 540 }
+    ]
+  },
+  {
+    "thumbnail": "res\\5\\1.jpg",
+    "title": "Sec. 9c, Chandigrah",
+    "slides": [
+      { "src": "res\\5\\1.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\10.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\2.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\3.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\4.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\5.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\6.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\7.jpg", "w": 540, "h": 960 },
+      { "src": "res\\5\\8.jpg", "w": 960, "h": 540 },
+      { "src": "res\\5\\9.jpg", "w": 960, "h": 540 }
+    ]
+  },
+  {
+    "thumbnail": "res\\6\\1.jpg",
+    "title": "Wave Estate, Sec. 85, Block D 71, Mohali",
+    "slides": [
+      { "src": "res\\6\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\6\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\6\\3.jpg", "w": 960, "h": 720 },
+      { "src": "res\\6\\4.jpg", "w": 720, "h": 960 },
+      { "src": "res\\6\\5.jpg", "w": 960, "h": 720 },
+      { "src": "res\\6\\6.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\7\\1.jpg",
+    "title": "Villa, Naldera, Shimla",
+    "slides": [
+      { "src": "res\\7\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\7\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\7\\3.jpg", "w": 960, "h": 540 },
+      { "src": "res\\7\\4.jpg", "w": 960, "h": 540 },
+      { "src": "res\\7\\5.jpg", "w": 960, "h": 540 }
+    ]
+  },
+  {
+    "thumbnail": "res\\8\\1.jpg",
+    "title": "Sec. 104/ EP 48, EMR Mohali Hills",
+    "slides": [
+      { "src": "res\\8\\1.jpg", "w": 727, "h": 960 },
+      { "src": "res\\8\\2.jpg", "w": 960, "h": 540 },
+      { "src": "res\\8\\3.jpg", "w": 960, "h": 540 },
+      { "src": "res\\8\\4.jpg", "w": 960, "h": 540 },
+      { "src": "res\\8\\5.jpg", "w": 960, "h": 540 }
+    ]
+  },
+  {
+    "thumbnail": "res\\9\\1.jpg",
+    "title": "Sachi Developers, Kharar, Mohali",
+    "slides": [
+      { "src": "res\\9\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\9\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\9\\3.jpg", "w": 960, "h": 720 },
+      { "src": "res\\9\\4.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\10\\1.jpg",
+    "title": "Old Bus Stand, Shimla",
+    "slides": [
+      { "src": "res\\10\\1.jpg", "w": 720, "h": 960 },
+      { "src": "res\\10\\2.jpg", "w": 720, "h": 960 },
+      { "src": "res\\10\\3.jpg", "w": 720, "h": 960 },
+      { "src": "res\\10\\4.jpg", "w": 720, "h": 960 },
+      { "src": "res\\10\\5.jpg", "w": 720, "h": 960 },
+      { "src": "res\\10\\6.jpg", "w": 720, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\11\\1.jpg",
+    "title": "Airocity Mohali",
+    "slides": [
+      { "src": "res\\11\\1.jpg", "w": 960, "h": 540 },
+      { "src": "res\\11\\2.jpg", "w": 540, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\12\\1.jpg",
+    "title": "Mandi, Himachal",
+    "slides": [
+      { "src": "res\\12\\1.jpg", "w": 960, "h": 540 },
+      { "src": "res\\12\\2.jpg", "w": 960, "h": 540 },
+      { "src": "res\\12\\3.jpg", "w": 540, "h": 960 },
+      { "src": "res\\12\\4.jpg", "w": 540, "h": 960 },
+      { "src": "res\\12\\5.jpg", "w": 540, "h": 960 },
+      { "src": "res\\12\\6.jpg", "w": 540, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\13\\1.jpg",
+    "title": "Rajpura",
+    "slides": [
+      { "src": "res\\13\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\13\\2.jpg", "w": 720, "h": 960 },
+      { "src": "res\\13\\3.jpg", "w": 720, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\14\\1.jpg",
+    "title": "Tanda Road, Dharamshala",
+    "slides": [
+      { "src": "res\\14\\1.jpg", "w": 540, "h": 960 },
+      { "src": "res\\14\\2.jpg", "w": 720, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\15\\1.jpg",
+    "title": "New vistas guest house, hadimba temple road, Manali",
+    "slides": [
+      { "src": "res\\15\\1.jpg", "w": 720, "h": 960 },
+      { "src": "res\\15\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\15\\3.jpg", "w": 720, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\16\\1.jpg",
+    "title": "MAdventure resort, Kufri, Shimla",
+    "slides": [
+      { "src": "res\\16\\1.jpg", "w": 720, "h": 960 },
+      { "src": "res\\16\\2.jpg", "w": 720, "h": 960 },
+      { "src": "res\\16\\3.jpg", "w": 960, "h": 720 },
+      { "src": "res\\16\\4.jpg", "w": 960, "h": 720 },
+      { "src": "res\\16\\5.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\17\\1.jpg",
+    "title": "Gurunanak Dev Bhavan, Ludhiana",
+    "slides": [
+      { "src": "res\\17\\1.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\10.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\2.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\3.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\4.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\5.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\6.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\7.jpg", "w": 720, "h": 960 },
+      { "src": "res\\17\\8.jpg", "w": 960, "h": 720 },
+      { "src": "res\\17\\9.jpg", "w": 720, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\18\\1.jpg",
+    "title": "Expo",
+    "slides": [
+      { "src": "res\\18\\1.jpg", "w": 960, "h": 639 },
+      { "src": "res\\18\\2.jpg", "w": 960, "h": 639 },
+      { "src": "res\\18\\3.jpg", "w": 960, "h": 639 },
+      { "src": "res\\18\\4.jpg", "w": 960, "h": 639 },
+      { "src": "res\\18\\5.jpg", "w": 639, "h": 960 },
+      { "src": "res\\18\\6.jpg", "w": 960, "h": 639 },
+      { "src": "res\\18\\7.jpg", "w": 639, "h": 960 },
+      { "src": "res\\18\\8.jpg", "w": 960, "h": 639 }
+    ]
+  },
+  {
+    "thumbnail": "res\\19\\1.jpg",
+    "title": "Eco city 5068, New Chandigrah",
+    "slides": [
+      { "src": "res\\19\\1.jpg", "w": 1080, "h": 492 },
+      { "src": "res\\19\\2.jpg", "w": 640, "h": 1403 },
+      { "src": "res\\19\\3.jpg", "w": 640, "h": 1403 },
+      { "src": "res\\19\\4.jpg", "w": 640, "h": 1396 }
+    ]
+  },
+  {
+    "thumbnail": "res\\20\\1.jpg",
+    "title": "Plot no 1287 sec 6 eco city, New Chandigrah",
+    "slides": [
+      { "src": "res\\20\\1.jpg", "w": 1080, "h": 496 },
+      { "src": "res\\20\\2.jpg", "w": 1080, "h": 496 }
+    ]
+  },
+  {
+    "thumbnail": "res\\21\\1.jpg",
+    "title": "Gurudwara Sahib Daba, Ludhiana",
+    "slides": [
+      { "src": "res\\21\\1.jpg", "w": 493, "h": 674 },
+      { "src": "res\\21\\2.jpg", "w": 892, "h": 960 },
+      { "src": "res\\21\\3.jpg", "w": 960, "h": 888 },
+      { "src": "res\\21\\4.jpg", "w": 1040, "h": 493 },
+      { "src": "res\\21\\5.jpg", "w": 959, "h": 960 },
+      { "src": "res\\21\\6.jpg", "w": 1040, "h": 493 }
+    ]
+  },
+  {
+    "thumbnail": "res\\22\\1.jpg",
+    "title": "Reliance Market, Kharar",
+    "slides": [
+      { "src": "res\\22\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\22\\2.jpg", "w": 1024, "h": 473 },
+      { "src": "res\\22\\3.jpg", "w": 960, "h": 720 },
+      { "src": "res\\22\\4.jpg", "w": 960, "h": 720 },
+      { "src": "res\\22\\5.jpg", "w": 960, "h": 720 },
+      { "src": "res\\22\\6.jpg", "w": 960, "h": 720 },
+      { "src": "res\\22\\7.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\23\\1.jpg",
+    "title": "Spring Field School, Yamunanagar",
+    "slides": [{ "src": "res\\23\\1.jpg", "w": 1024, "h": 473 }]
+  },
+  {
+    "thumbnail": "res\\24\\1.jpg",
+    "title": "loral epsum",
+    "slides": [
+      { "src": "res\\24\\1.jpg", "w": 1080, "h": 497 },
+      { "src": "res\\24\\2.jpg", "w": 640, "h": 1403 },
+      { "src": "res\\24\\3.jpg", "w": 1080, "h": 497 },
+      { "src": "res\\24\\4.jpg", "w": 1080, "h": 497 },
+      { "src": "res\\24\\5.jpg", "w": 1080, "h": 497 }
+    ]
+  },
+  {
+    "thumbnail": "res\\25\\1.jpg",
+    "title": "UPVC with glass house",
+    "slides": [
+      { "src": "res\\25\\1.jpg", "w": 639, "h": 475 },
+      { "src": "res\\25\\2.jpg", "w": 960, "h": 720 },
+      { "src": "res\\25\\3.jpg", "w": 640, "h": 1386 },
+      { "src": "res\\25\\4.jpg", "w": 640, "h": 1386 },
+      { "src": "res\\25\\5.jpg", "w": 640, "h": 1386 }
+    ]
+  },
+  {
+    "thumbnail": "res\\26\\1.jpg",
+    "title": "Lasoi pind, Ludhiana, Punjab",
+    "slides": [
+      { "src": "res\\26\\1.jpg", "w": 960, "h": 564 },
+      { "src": "res\\26\\2.jpg", "w": 662, "h": 960 },
+      { "src": "res\\26\\3.jpg", "w": 720, "h": 960 },
+      { "src": "res\\26\\4.jpg", "w": 720, "h": 960 },
+      { "src": "res\\26\\5.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\27\\1.jpg",
+    "title": "Dhalpur Kullu",
+    "slides": [
+      { "src": "res\\27\\1.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\2.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\3.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\4.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\5.jpg", "w": 960, "h": 721 },
+      { "src": "res\\27\\6.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\7.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\8.jpg", "w": 540, "h": 960 },
+      { "src": "res\\27\\9.jpg", "w": 540, "h": 960 }
+    ]
+  },
+  {
+    "thumbnail": "res\\28\\1.jpg",
+    "title": "Shimla",
+    "slides": [
+      { "src": "res\\28\\1.jpg", "w": 960, "h": 541 },
+      { "src": "res\\28\\2.jpg", "w": 960, "h": 541 },
+      { "src": "res\\28\\3.jpg", "w": 960, "h": 541 },
+      { "src": "res\\28\\4.jpg", "w": 960, "h": 720 },
+      { "src": "res\\28\\5.jpg", "w": 960, "h": 720 },
+      { "src": "res\\28\\6.jpg", "w": 960, "h": 541 },
+      { "src": "res\\28\\7.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\29\\1.jpg",
+    "title": "Prem Vidhya bhawan, near SSB gate, police colony, kasumpti, Shimla 9",
+    "slides": [
+      { "src": "res\\29\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\29\\10.jpg", "w": 960, "h": 720 },
+      { "src": "res\\29\\2.jpg", "w": 720, "h": 960 },
+      { "src": "res\\29\\3.jpg", "w": 720, "h": 960 },
+      { "src": "res\\29\\4.jpg", "w": 960, "h": 720 },
+      { "src": "res\\29\\5.jpg", "w": 720, "h": 960 },
+      { "src": "res\\29\\6.jpg", "w": 720, "h": 960 },
+      { "src": "res\\29\\7.jpg", "w": 720, "h": 960 },
+      { "src": "res\\29\\8.jpg", "w": 720, "h": 960 },
+      { "src": "res\\29\\9.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\30\\1.jpg",
+    "title": "Morni hills, Thandog, Haryana",
+    "slides": [
+      { "src": "res\\30\\1.jpg", "w": 960, "h": 960 },
+      { "src": "res\\30\\10.jpg", "w": 960, "h": 720 },
+      { "src": "res\\30\\11.jpg", "w": 960, "h": 720 },
+      { "src": "res\\30\\12.jpg", "w": 960, "h": 720 },
+      { "src": "res\\30\\13.jpg", "w": 960, "h": 720 },
+      { "src": "res\\30\\2.jpg", "w": 960, "h": 960 },
+      { "src": "res\\30\\3.jpg", "w": 960, "h": 960 },
+      { "src": "res\\30\\4.jpg", "w": 960, "h": 960 },
+      { "src": "res\\30\\5.jpg", "w": 960, "h": 960 },
+      { "src": "res\\30\\6.jpg", "w": 960, "h": 720 },
+      { "src": "res\\30\\7.jpg", "w": 960, "h": 960 },
+      { "src": "res\\30\\8.jpg", "w": 960, "h": 720 },
+      { "src": "res\\30\\9.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\31\\1.jpg",
+    "title": "Sagar Hotel TDI",
+    "slides": [{ "src": "res\\31\\1.jpg", "w": 1080, "h": 1080 }]
+  },
+  {
+    "thumbnail": "res\\32\\1.jpg",
+    "title": "PMN college, Rajpura",
+    "slides": [
+      { "src": "res\\32\\1.jpg", "w": 1080, "h": 495 },
+      { "src": "res\\32\\2.jpg", "w": 1080, "h": 495 },
+      { "src": "res\\32\\3.jpg", "w": 1080, "h": 493 },
+      { "src": "res\\32\\4.jpg", "w": 1080, "h": 493 },
+      { "src": "res\\32\\5.jpg", "w": 1080, "h": 493 },
+      { "src": "res\\32\\6.jpg", "w": 1080, "h": 497 },
+      { "src": "res\\32\\7.jpg", "w": 1080, "h": 497 }
+    ]
+  },
+  {
+    "thumbnail": "res\\33\\1.jpg",
+    "title": "Village Bela, Ludhiana",
+    "slides": [
+      { "src": "res\\33\\1.jpg", "w": 960, "h": 773 },
+      { "src": "res\\33\\2.jpg", "w": 960, "h": 652 },
+      { "src": "res\\33\\3.jpg", "w": 960, "h": 718 }
+    ]
+  },
+  {
+    "thumbnail": "res\\34\\1.jpg",
+    "title": "Sector 39, Chandigarh",
+    "slides": [
+      { "src": "res\\34\\1.jpg", "w": 960, "h": 720 },
+      { "src": "res\\34\\2.jpg", "w": 960, "h": 720 }
+    ]
+  },
+  {
+    "thumbnail": "res\\35\\1.jpg",
+    "title": "Surya Enclave, Jalandhar",
+    "slides": [
+      { "src": "res\\35\\1.jpg", "w": 1080, "h": 497 },
+      { "src": "res\\35\\2.jpg", "w": 640, "h": 1403 },
+      { "src": "res\\35\\3.jpg", "w": 640, "h": 1403 },
+      { "src": "res\\35\\4.jpg", "w": 640, "h": 1403 },
+      { "src": "res\\35\\5.jpg", "w": 1080, "h": 497 }
+    ]
+  }
+];
